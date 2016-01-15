4ac3f81b654bae963ffd2a1e6c902403c904d91a
Tomi Knuuttila <tomi.knuuttila@gmail.com>
2016-01-15 16:22:31 +0200

    Added the devpcap.py file, basically the same thing that pcap_exam but this one prints log file, the formating should be fixed tough

---
+++ devpcap.py
@@ -0,0 +1,39 @@
+
+import getopt, sys
+import dpkt, pcap
+
+def usage():
+    print >>sys.stderr, 'usage: %s [-i device] [pattern]' % sys.argv[0]
+    sys.exit(1)
+
+def main():
+    opts, args = getopt.getopt(sys.argv[1:], 'i:h')
+    name = None
+    for o, a in opts:
+        if o == '-i': name = a
+        else: usage()
+	
+    f = open('pcaplog.txt' , 'w')
+        
+    pc = pcap.pcap(name)
+    pc.setfilter(' '.join(args))
+    decode = { pcap.DLT_LOOP:dpkt.loopback.Loopback,
+               pcap.DLT_NULL:dpkt.loopback.Loopback,
+               pcap.DLT_EN10MB:dpkt.ethernet.Ethernet }[pc.datalink()]
+    try:
+        print 'listening on %s: %s' % (pc.name, pc.filter)
+        for ts, pkt in pc:
+            print ts, `decode(pkt)`
+	    packet = decode(pkt)
+	    s = str(packet)
+	    f.write(str(ts))
+	    f.write('\t')
+	    f.write(s)
+	    f.write('\n')
+    except KeyboardInterrupt:
+        nrecv, ndrop, nifdrop = pc.stats()
+        print '\n%d packets received by filter' % nrecv
+        print '%d packets dropped by kernel' % ndrop
+
+if __name__ == '__main__':
+    main()
