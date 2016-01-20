3bbd7fa7e08a7e72376630d7043b861b35d408a1
Mika Leppiaho <mika.leppiaho@gmail.com>
2016-01-20 08:59:17 +0000

    Fixed typos and color

--- piShark/main.py
+++ piShark/main.py
@@ -5,7 +5,7 @@ class MainWidget(Widget):
 	my_data = ListProperty([])
 	selected_value = StringProperty('Select a button')
 	def change(self,change):
-		self.selected_value = 'Selected: {]'.format(change.text)
+		self.selected_value = 'Selected: {}'.format(change.text)
 class PiSharkApp(App):
 		def build(self):
 			return MainWidget()
