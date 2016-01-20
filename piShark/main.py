69500907db4c2dbd99cf2b1109a12cc3719d4fe4
Mika Leppiaho <mika.leppiaho@gmail.com>
2016-01-20 08:12:30 +0000

    Added input

--- piShark/main.py
+++ piShark/main.py
@@ -1,9 +1,11 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-
+from kivy.properties import ListProperty, StringProperty
 class MainWidget(Widget):
-	pass
-
+	my_data = ListProperty([])
+	selected_value = StringProperty('Select a button')
+	def change(self,change):
+		self.selected_value = 'Selected: {]'.format(change.text)
 class PiSharkApp(App):
 		def build(self):
 			return MainWidget()
