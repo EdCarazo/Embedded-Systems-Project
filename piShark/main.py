7b01cda8ddec28b267e252ed158760f717380cce
Mika Leppiaho <mika.leppiaho@gmail.com>
2016-01-22 15:02:06 +0000

    Added sending message with the selected protocol and ip trough the pipe

--- piShark/main.py
+++ piShark/main.py
@@ -1,16 +1,51 @@
+import protocol
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import ListProperty, StringProperty
+from kivy.properties import ListProperty, StringProperty, NumericProperty
+import os
+readPipe = "/tmp/pipe1"
+writePipe = "/tmp/pipe2"
+try:
+	os.mkfifo(readPipe)
+	os.mkfifo(writePipe)
+except OSError:
+	pass
 class MainWidget(Widget):
 	my_data = ListProperty([])
 	selected_value = StringProperty('Select a button')
+	def MMS(self, *args):
+		if args[1]== "down":
+			protocol.protocol = '1'
+			print protocol.protocol
+	def GOOSE(self, *args):
+		if args[1]== "down":
+			protocol.protocol = '2'
+			print protocol.protocol
+	def SV(self, *args):
+		if args[1]== "down":
+			protocol.protocol = '3'
+			print protocol.protocol
+				
 	def change(self,change):
 		self.selected_value = 'Selected: {}'.format(change.text)
+	
+
 	def teejotain(self):
-		self.ids.nappi.text = 'Jotain'
+		self.ids.start.text = 'Started capture with filter'
+		src = self.ids.src.text
+		dst = self.ids.dst.text
+		message = protocol.protocol+","+src+","+dst
+		#print message
+		f = open(writePipe, 'w')
+		f.write(message)
+		f.close()
+	def teejotain2(self):
+		self.ids.start.text = 'Start'
 class PiSharkApp(App):
 		def build(self):
 			return MainWidget()
 
 if __name__ == '__main__':
 	PiSharkApp().run()
+
+
