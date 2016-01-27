4d0c48036153e6fb8f189de51b6c25a68efaa89f
JussiKunnari <jusbaaja11@gmail.com>
2016-01-27 08:25:16 +0000

    Local backup

--- piShark/main.py
+++ piShark/main.py
@@ -1,14 +1,53 @@
-from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.properties import ListProperty, StringProperty
+from kivy.app import App 
+from kivy.uix.widget import Widget 
+from kivy.properties import ListProperty, StringProperty, NumericProperty
+protocol = '0'
+#import os
+#import posix_ipc
+
+#writePipe = "/tmp/pipe"
+#messageQueue = "/msg_que"
+
+#try:
+#	os.mkfifo(writePipe)
+#except OSError:
+#	pass
+
 class MainWidget(Widget):
 	my_data = ListProperty([])
-	selected_value = StringProperty('Select a button')
+	selected_value = StringProperty('Select a packet')
+#	mq = posix_ipc.MessageQueue(messageQueue)
 	def change(self,change):
 		self.selected_value = 'Selected: {}'.format(change.text)
+
+	def protocol1(self):
+		global protocol
+		protocol = '1'
+	def protocol2(self):
+		global protocol
+		protocol = '2'
+	def protocol3(self):
+		global protocol
+		protocol = '3'
+
+	def teejotain(self):
+		self.ids.start.text = 'Started capture with filter'
+		src = self.ids.src.text
+		dst = self.ids.dst.text		
+		global protocol
+		message =protocol+","+src+","+dst
+		print message
+#		f, _ = mq.receive()
+		self.my_data.append(message)
+	def teejotain2(self):
+		self.ids.start.text = 'Start'
+#			mq.close()
+
 class PiSharkApp(App):
 		def build(self):
 			return MainWidget()
 
 if __name__ == '__main__':
 	PiSharkApp().run()
+
+
