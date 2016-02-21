d366ba57e1ab0732e951989954e33bda7b60ebd3
Pitkis <sami.pitkakangas@email.com>
2016-02-21 19:30:38 +0000

    Added notification bar with status information, fixed crashing while not getting data

--- piShark/main.py
+++ piShark/main.py
@@ -44,14 +44,20 @@ class TriggeredCapture(Screen):
 		protocol = '4'
 	def receive(self, *args):
 		global count
-		f, _ = mq.receive()
-		f_string = str(f)
-		f_list = f_string.split(',')
-		self.my_data3.append(f_list[1])
-		self.my_data4.append(f_list[2])
-		self.my_data5.append(f_list[3])
-		self.my_data6.append(f_list[4])
-		self.my_data7.append(f_list[5])
+		try:
+			f, _ = mq.receive(10)
+			f_string = str(f)
+			f_list = f_string.split(',')
+			self.ids.noti.text = 'Running'
+			self.my_data1.append(f_list[0])
+			self.my_data3.append(f_list[2])
+			self.my_data4.append(f_list[3])
+			self.my_data5.append(f_list[4])
+			self.my_data6.append(f_list[5])
+			self.my_data7.append(f_list[6])
+		except:
+			self.ids.noti.text = 'Nothing received'
+			pass
 	def send_parameters(self, params):
 		p = open(writePipe, 'w')
 		params_send = str(params)
@@ -61,18 +67,24 @@ class TriggeredCapture(Screen):
 		global cls
 		cls = '0'
 		self.ids.start.text = 'Started capture with filter'
+		self.ids.noti.text = 'Starting capture'
 		src = self.ids.src.text
 		dst = self.ids.dst.text
+		amount=self.ids.amount.text
+		if self.ids.amount.text=="":
+			amount="0"
 		global protocol
-		params = protocol+","+src+","+dst
+		params = protocol+","+src+","+dst+","+amount
 		self.send_parameters(params)
 		print params
 		Clock.schedule_interval(self.receive, 1/1000.)
+
 	def stop(self):
 		global cls
 		global count
 		if cls == '0':
 			cls = '1'
+			self.ids.noti.text = 'Click Start to Continue or Stop to Clear'
 			self.ids.start.text = 'Start'
 			Clock.unschedule(self.receive)
 		elif cls == '1':
@@ -83,6 +95,7 @@ class TriggeredCapture(Screen):
 			del self.my_data5[:]
 			del self.my_data6[:]
 			del self.my_data7[:]
+			self.ids.noti.text = 'Cleared'
 			cls = '0'
 
 
@@ -115,16 +128,20 @@ class BasicCapture(Screen):
                 protocol = '4'
 	def receive(self, *args):
 		global count
-		f, _ = mq.receive()
-		f_string = str(f)
-		f_list = f_string.split(',')
-		self.my_data3.append(f_list[1])
-		self.my_data4.append(f_list[2])
-		self.my_data5.append(f_list[3])
-		self.my_data6.append(f_list[4])
-		self.my_data7.append(f_list[5])
-
-		
+		try:
+			f, _ = mq.receive(10)
+			self.ids.noti.text = 'Running'
+			f_string = str(f)
+			f_list = f_string.split(',')
+			self.my_data1.append(f_list[0])
+			self.my_data3.append(f_list[2])
+			self.my_data4.append(f_list[3])
+			self.my_data5.append(f_list[4])
+			self.my_data6.append(f_list[5])
+			self.my_data7.append(f_list[6])
+		except:
+			self.ids.noti.text = 'Nothing received'
+			pass		
 	def send_parameters(self, params):
 		p = open(writePipe, 'w')
 		params_send = str(params)
@@ -135,10 +152,12 @@ class BasicCapture(Screen):
 		global cls
 		cls = '0'
 		self.ids.start.text = 'Started capture with filter'
+		self.ids.noti.text = 'Starting capture'
 		src = self.ids.src.text
 		dst = self.ids.dst.text
+		amount = "0"
 		global protocol
-		params = protocol+","+src+","+dst
+		params = protocol+","+src+","+dst+","+amount
 		self.send_parameters(params)
 		print params
 		Clock.schedule_interval(self.receive, 1/1000.)		
@@ -147,6 +166,7 @@ class BasicCapture(Screen):
 		global count	
 		if cls == '0':
 			cls = '1'
+			self.ids.noti.text = 'Click Start to Continue or Stop to Clear'
 			self.ids.start.text = 'Start'
 			Clock.unschedule(self.receive)					
 		elif cls == '1':			
@@ -157,6 +177,7 @@ class BasicCapture(Screen):
 			del self.my_data5[:]
 			del self.my_data6[:]
 			del self.my_data7[:]
+			self.ids.noti.text = 'Cleared'
 			cls = '0'
 presentation = Builder.load_file("pishark.kv")						
 class PiSharkApp(App):
