ea8c6187495cd0bd8168a9c82a3cdbe410c2f290
Matti Suominen <matsuominen@gmail.com>
2016-02-25 07:24:52 +0200

    Added Sleep

--- start.sh
+++ start.sh
@@ -1,8 +1,11 @@
 #
 # This script starts first back end and then front end
 #
+
 echo "Starting back end"
 sudo python new_format.py
+
+Sleep 1
 
 echo "Starting front end"
 sudo python piShark/main.py
